312f49a81de0c8c3dcc5402004e7dbc92e91a425
Durgesh <ds6636297@gmail.com>
2025-12-06 21:32:57 +0530

    created routes for user

---
+++ Backend/routes/user.route.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { 
+  register,
+  login,
+  logout,
+  refreshToken
+} from "../controller/user.controller.js";
+
+import { verifyJwt } from "../middleware/auth.middleware.js";
+const router=Router();
+
+router.post("/register",register);
+router.post("/login", login);
+router.post("/logout",verifyJwt, logout);
+router.post("/refreshtoken",refreshToken);
