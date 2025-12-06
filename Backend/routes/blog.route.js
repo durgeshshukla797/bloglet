f82544b72b76bce60e0ce1b24a11b5e548f7a1d5
Durgesh <ds6636297@gmail.com>
2025-12-06 21:23:33 +0530

    created routes for blog

---
+++ Backend/routes/blog.route.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { 
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  getPublicBlogs,
+  updateBlog,
+  deleteBlog 
+} from "../controller/blog.controller.js";
+
+import { upload } from "../middleware/multer.middleware.js";
+import { verifyJwt } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.post(
+  "/blogs",
+  verifyJwt,
+  upload.fields([{ name: "coverImage", maxCount: 1 }]),
+  createBlog
+);
+router.put(
+  "/blogs/:blogId",
+  verifyJwt,
+  upload.fields([{ name: "coverImage", maxCount: 1 }]),
+  updateBlog
+);
+
+router.get("/blogs", verifyJwt, getAllBlogs);
+router.get("/blogs/:blogId", getBlogById);
+router.get("/public-blogs", getPublicBlogs);
+router.delete("/blogs/:blogId", verifyJwt, deleteBlog);
+
+export default router;
